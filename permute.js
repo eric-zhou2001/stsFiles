cd6bb2f09bb610b06d8f4f4877a3b5171ac52e34
ericzhzhou2001@gmail.com <ericzhzhou2001@gmail.com>
2020-09-18 19:13:40 -0400

    fixed permuteMultiply

---
+++ permute.js
@@ -0,0 +1,15 @@
+var permutations = []
+
+function permute(num1,num2,num3){
+    if (num2==num1) {
+        var digits = num3.split("");
+        permutations.push(digits);
+        return;
+    }
+    for (var i=0; i<num1; i++) {
+        permute(num1,num2+1,num3+i);
+    }
+}              
+permute(3,0,"");
+
+console.log(permutations);
--- permuteMultiply.js
+++ permuteMultiply.js
@@ -1,9 +1,24 @@
-var permutations = []
+var indices = [];
+var permutationProduct = [];
+var permutationSum = [];
 
+// Creates the permutationSum, i.e., AAA, AAA + AAB, etc...
+function sum(numPermutations) {
+    var total = 0;
+    for (var i = 0; i < numPermutations; ++i) {
+        total += permutationProduct[i];
+    }
+    permutationSum.push(total);
+}
+
+// Create the permutationProducts according to the master index array.
 function multiply(digits) {
-    var product = 1;
-    for (var i = 0; i < digits.length; ++i) {
-        product = product * digits[i];
+    for (var i = 0; i < indices.length; ++i) {
+        var product = 1;
+        for (var j = 0; j < indices[i].length; ++j) {
+            product = product * digits[indices[i][j]];
+        }
+        permutationProduct.push(product);
     }
     return product;
 }
@@ -11,14 +26,26 @@ function multiply(digits) {
 function permute(num1,num2,num3){
     if (num2==num1) {
         var digits = num3.split("");
-        product = multiply(digits);
-        permutations.push(product);
+        indices.push(digits);
         return;
     }
     for (var i=0; i<num1; i++) {
         permute(num1,num2+1,num3+i);
     }
-}              
-permute(4,0,"");
+}
+// var input = prompt("What is the input?");
+// Uncomment above when testing with photoshop
 
-console.log(permutations);
+// For testing purposes now, we can start to change the input array manually.
+var input = [1,4,7];
+
+// Creates master index
+permute(input.length, 0,"");
+
+// Create permutation products according to master index
+multiply(input);
+
+for (var i=0; i<indices.length; ++i) {
+    sum(i+1);
+}
+console.log(permutationSum);
