fd028dd7919867e7c1816cbe4bc2ea4e22b8c027
ericzhzhou2001@gmail.com <ericzhzhou2001@gmail.com>
2020-10-08 22:55:40 -0400

    Added beginning of JS exercises

---
+++ js_workshop/Syntax/syntax.js
@@ -0,0 +1,37 @@
+/**
+ * Instructor note: this page should be left blank, but we will have notes here.
+ * 
+ * Exercise 1: Variables and Boxes
+ * Give introduction of Variables, as well as a quick recap since the students 
+ * should have a somewhat decent grasp of it already.
+ * 
+ * Demo:
+ * var x = 10;
+ * console.log(x);
+ * x = 20;
+ * console.log(x);
+ * 
+ * Make them type this out, look at the results. We can refer to pieces of data 
+ * not explicitly of course! This is a PRIMITIVE.
+ * 
+ * -------------------------------------------------------------------------------
+ * 
+ * Exercise 1.1: Keyword Declarations
+ * Let the students type out "let" vs "var" vs "const." An example:
+ * 
+ * let x = 20;
+ * var y = 25;
+ * const z = 30;
+ * 
+ * console.log(x);
+ * console.log(y);
+ * console.log(z);
+ * 
+ * Notice how these all work in terms of declaring a variable. Don't go into too
+ * much detail, since this is extremely confusing and to be honest I personally
+ * don't know the exact specifics either. Just point out how these do exist and
+ * mention how const means that we can no longer change the value stored (do a
+ * z = 34958903? show it doesn't work). 
+ * 
+ * Stick with var.
+ */
