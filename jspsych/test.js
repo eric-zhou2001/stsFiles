c1c754ef815ca0970ec02742000a54210c5be0e4
Eric Zhou <ericzhzhou2001@gmail.com>
2020-11-04 21:02:52 -0600

    transfers files to windows

---
+++ jspsych/test.js
@@ -0,0 +1,28 @@
+var timeline = [];
+
+var welcome = {
+    type: 'html-keyboard-response',
+    stimulus: 'Welcome to the experiment. Press any key to begin.'
+};
+
+timeline.push(welcome);
+
+var instructions = {
+    type: "html-keyboard-response",
+    stimulus: "<p>In this experiment, a circle will appear in the center " +
+        "of the screen.</p><p>If the circle is <strong>blue</strong>, " +
+        "press the letter F on the keyboard as fast as you can.</p>" +
+        "<p>If the circle is <strong>orange</strong>, press the letter J " +
+        "as fast as you can.</p>" +
+        "<div style='width: 700px;'>"+
+        "<div style='float: left;'><img src='img/blue.png'></img>" +
+        "<p class='small'><strong>Press the F key</strong></p></div>" +
+        "<div class='float: right;'><img src='img/orange.png'></img>" +
+        "<p class='small'><strong>Press the J key</strong></p></div>" +
+        "</div>"+
+        "<p>Press any key to begin.</p>"
+  };
+
+jsPsych.init({
+    timeline: timeline
+});
