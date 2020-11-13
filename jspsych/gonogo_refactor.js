b1e310e184542a6a4292cbcfcc2f0f055e8b3a7d
ericzhzhou2001@gmail.com <ericzhzhou2001@gmail.com>
2020-11-12 22:30:47 -0600

    added 11/12 updates for jspsych proj

---
+++ jspsych/gonogo_refactor.js
@@ -0,0 +1,110 @@
+timeline = [];
+
+/**
+ * Consider changing not to timeline variables but to somethign else. Timeline
+ * variables forces the experiment to go through everything. This might be good for
+ * cues, but not for targets where we only want one target displayed.
+ */
+var target = {
+    type: "image-keyboard-response",
+    stimulus: jsPsych.timelineVariable('target'),
+    choices: [37, 39],
+    on_finish: function(data) {
+      // find a way to make this different, depending on left/right.
+      console.log("image displayed: " + jsPsych.timelineVariable('target',true));
+      var photo = jsPsych.timelineVariable('target',true);
+      var correctDirection = 0;
+
+      // Checks the photo to determine what arrow key is necessary.
+      if (photo == "./img/left_button.jpg") {
+          correctDecision = 37;
+      } else {
+          correctDecision = 39;
+      }
+
+      // Sets data to correct/incorrect depending on user choice.
+      console.log(data);
+      if (data.key_press == correctDecision) {
+        console.log("Correct decision. Setting data to be true.");
+        data.correct = true;
+      } else {
+        console.log("Incorrect decision. Setting data to be false.");
+        data.correct = false;
+      }
+    }
+}
+
+// Will be set to true after trial runs.
+var trial_finished_target = false;
+var target_node = {
+    timeline: [target],
+    timeline_variables: [
+        {target: "./img/left_button.jpg"},
+        {target: "./img/right_button.jpg"}
+    ],
+    conditional_function: () => {
+      if (trial_finished_target) {
+        console.log("Trial has finished.");
+        trial_finished_target = false;
+        return false;
+      } else {
+        console.log("Trial has not occured yet.");
+        trial_finished_target = true;
+        return true;
+      }
+    }
+}   
+
+var fixation = {
+  type: 'html-keyboard-response',
+  stimulus: '<div style="font-size:60px;">+</div>',
+  choices: jsPsych.NO_KEYS,
+  trial_duration: function(){
+    return jsPsych.randomization.sampleWithoutReplacement([250, 500, 750, 1000, 1250, 1500, 1750, 2000], 1)[0];
+  },
+  data: {test_part: 'fixation'}
+};
+
+var feedback_node = {
+  timeline: [fixation],
+  conditional_function: function() {
+    // .last gets the last 2 results (target, fixation). [0] returns the 
+    // target, which we need to access the result of.
+    var last_trial_correct = jsPsych.data.get().last(1).values()[0].correct;
+    var random = Math.random() * 10; // Check the exact chance. 
+    console.log(random);
+    if (random < 8) {
+      // Randomization to see if to display feedback.
+      if (last_trial_correct) {
+        console.log("Adding to timeline.");
+        var feedback = {
+          type: "image-keyboard-response",
+          stimulus: "./thumb_up.png",
+          choices: jsPsych.NO_KEYS,
+          trial_duration: 1000
+        };
+        timeline.push(feedback);
+      } else {
+        var feedback = {
+          type: "image-keyboard-response",
+          stimulus: "./thumbs_down.png",
+          choices: jsPsych.NO_KEYS,
+          trial_duration: 1000
+        };
+        timeline.push(feedback);
+      }
+    } else {
+      console.log("not adding to timeline. 20%");
+    }
+    return true;
+  }
+}
+
+timeline.push(target_node,feedback_node);
+
+jsPsych.init({
+    timeline: timeline,
+    on_finish: function() {
+        jsPsych.data.displayData();
+    }
+});
--- jspsych/test.js
+++ jspsych/test.js
@@ -7,22 +7,117 @@ var welcome = {
 
 timeline.push(welcome);
 
-var instructions = {
-    type: "html-keyboard-response",
-    stimulus: "<p>In this experiment, a circle will appear in the center " +
-        "of the screen.</p><p>If the circle is <strong>blue</strong>, " +
-        "press the letter F on the keyboard as fast as you can.</p>" +
-        "<p>If the circle is <strong>orange</strong>, press the letter J " +
-        "as fast as you can.</p>" +
-        "<div style='width: 700px;'>"+
-        "<div style='float: left;'><img src='img/blue.png'></img>" +
-        "<p class='small'><strong>Press the F key</strong></p></div>" +
-        "<div class='float: right;'><img src='img/orange.png'></img>" +
-        "<p class='small'><strong>Press the J key</strong></p></div>" +
-        "</div>"+
-        "<p>Press any key to begin.</p>"
-  };
+var yn_trial = {
+    type: 'html-keyboard-response',
+    stimulus: "<p>Press N, don't press Y</p>",
+    choices: [89, 78], // y/n
+    on_finish: function (data) {
+        if (data.key_press == 78) {
+            data.correct = true;
+            console.log("Pressed n.");
+            
+        } else {
+            data.correct = false;
+            console.log("pressed Y");
+        }
+    },
+};
+
+var feedback = {
+    type: 'image-keyboard-response',
+    stimulus: jsPsych.timelineVariable('img'),
+};
+
+var color_timeline = [
+    { img: "./blue.png" },
+    { img: "./orange.png" },
+];
+
+// This is exactly what we are god damn looking for!!
+var feedback_node = {
+    timeline: [feedback],
+    conditional_function: function() {
+        var last_trial_correct = jsPsych.data.get().last(1).values()[0].correct;
+        var random = Math.random() * 10; // Check the exact chance. 
+        console.log(random);
+        if (last_trial_correct && random < 8) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    timeline_variables: color_timeline,
+    randomize_order: true
+}
+
+var after_if_trial = {
+    type: 'html-keyboard-response',
+    stimulus: 'This is the trial after the conditional.'
+}
+
+timeline.push(yn_trial, feedback_node, after_if_trial);
+
+// var last_trial_correct = jsPsych.data.get().last(1).values()[0].correct;
+// var random = Math.random() * 10; // Check the exact chance. 
+// console.log(random);
+// if (last_trial_correct && random < 8) {
+//     var feedback = {
+//         type: 'image-keyboard-response',
+//         stimulus: "",
+//     };
+//     timeline.push(feedback);
+// }
+
+
+
+// var feedback = {
+//     type: "html-keyboard-response",
+//     stimulus: function () {
+//         var last_trial_correct = jsPsych.data.get().last(1).values()[0].correct;
+//         var random = Math.random() * 10; // Check the exact chance. 
+//         console.log(random);
+//         if (last_trial_correct && random < 8) {
+//             return "<p>Correct!</p>"
+//         } else {
+//             return "<p>Wrong.</p>"
+//         }
+//     }
+// }
+
+// Arrow example below (37 for left, 39 for right. JavaScipit)
+// var yn_trial = {
+//     type: 'html-keyboard-response',
+//     stimulus: "<p>Press left arrow key, don't press right arrow key</p>",
+//     choices: [37, 39], // <-- 37, --> 39
+//     on_finish: function(data) {
+//         if (data.key_press == 37) {
+//             data.correct = true;
+//             console.log("Pressed left arrow.");
+//         } else {
+//             data.correct = false;
+//             console.log("pressed right arrow");
+//         }
+//     },
+// };
+
+// var feedback = {
+//     type: "html-keyboard-response",
+//     stimulus: function () {
+//         var last_trial_correct = jsPsych.data.get().last(1).values()[0].correct;
+//         var random = Math.random() * 10; // Check the exact chance.
+//         console.log(random);
+//         if (last_trial_correct && random < 8) {
+//             return "<p>Correct!</p>"
+//         } else {
+//             return "<p>Wrong.</p>"
+//         }
+//     }
+// }
+
 
 jsPsych.init({
-    timeline: timeline
+    timeline: timeline,
+    on_finish: function() {
+        jsPsych.data.displayData();
+    }
 });
